3fb3037ffb44f591c15762e161c3f07aa8b0cd48
vlatan <33163699+vlatan@users.noreply.github.com>
2025-05-23 19:05:48 +0200

    Remove CORS

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -21,29 +21,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	mux.HandleFunc("GET /health", s.healthHandler)
 	mux.HandleFunc("GET /static/", s.staticHandler)
 
-	// Wrap the mux with CORS middleware
-	// return s.corsMiddleware(mux)
-
 	return mux
-}
-
-func (s *Server) corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Set CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Replace "*" with specific origins if needed
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-CSRF-Token")
-		w.Header().Set("Access-Control-Allow-Credentials", "false") // Set to "true" if credentials are required
-
-		// Handle preflight OPTIONS requests
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-
-		// Proceed with the next handler
-		next.ServeHTTP(w, r)
-	})
 }
 
 type HomePageData struct {
