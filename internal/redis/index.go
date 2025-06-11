b124b7d7c870a81364a8267b01f47423578d0cbc
vlatan <33163699+vlatan@users.noreply.github.com>
2025-06-11 12:26:32 +0200

    Add methods to service interface

--- internal/redis/index.go
+++ internal/redis/index.go
@@ -2,7 +2,9 @@ package redis
 
 import (
 	"context"
+	"factual-docs/internal/database"
 	"fmt"
+	"log"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -38,4 +40,32 @@ func (s *service) UpsertDocument(ctx context.Context, columns ...string) (int, e
 	}).Err()
 
 	return post.ID, err
+}
+
+func (s *service) DeleteDocument(ctx context.Context, id int) error {
+	// TODO: delete post method
+
+	// Remove from Redis
+	key := fmt.Sprintf("doc:%d", id)
+	return s.rdb.Del(ctx, key).Err()
+}
+
+func (s *service) SearchDocuments(ctx context.Context, query string) (posts []database.Post, err error) {
+	result, err := s.rdb.FTSearch(ctx, "docs", query).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: Convert the doc to Post object
+	for _, doc := range result.Docs {
+		log.Println(doc.Fields)
+		// posts = append(posts, doc.Fields)
+	}
+
+	return posts, nil
+}
+
+func (s *service) SyncExistingData(ctx context.Context) error {
+	// TODO: Clean way to sync the D and th index
+	return nil
 }
