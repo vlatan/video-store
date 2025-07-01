6183076fe0e9eb0bd836d70243f1e0251b6461c0
vlatan <33163699+vlatan@users.noreply.github.com>
2025-07-01 16:58:25 +0200

    Remove type

--- internal/users/utils.go
+++ internal/users/utils.go
@@ -2,11 +2,6 @@ package users
 
 import "database/sql"
 
-type Actions struct {
-	Liked bool
-	Faved bool
-}
-
 // Helper function to convert string pointer or empty string to sql.NullString
 func NullString(s *string) sql.NullString {
 	if s == nil || *s == "" {
