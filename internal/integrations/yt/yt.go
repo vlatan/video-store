0b195d2626c9474b26d62fc9436a432e365da010
vlatan <33163699+vlatan@users.noreply.github.com>
2025-07-06 18:01:48 +0200

    Start normalizing title

--- internal/integrations/yt/yt.go
+++ internal/integrations/yt/yt.go
@@ -52,6 +52,7 @@ func (s *Service) GetVideos(videoIDs ...string) ([]*youtube.Video, error) {
 // Validate a YouTube video against custom criteria.
 // Returns client facing error messages if any.
 func (s *Service) ValidateYouTubeVideo(video *youtube.Video) error {
+
 	if video.Status.PrivacyStatus == "private" {
 		return errors.New("this video is not public")
 	}
@@ -84,4 +85,72 @@ func (s *Service) ValidateYouTubeVideo(video *youtube.Video) error {
 	}
 
 	return nil
+}
+
+// Create post object
+func (s *Service) CreatePost(video *youtube.Video, playlistID string) *models.Post {
+	var post models.Post
+
+	post.Title = normalizeTitle(video.Snippet.Title)
+
+	return &post
+}
+
+// Normalize the YouTube video title
+func normalizeTitle(title string) string {
+
+	// Cut off the title at certain substrings
+	for _, substring := range []string{" I SLICE ", " // ", " | "} {
+		title = strings.Split(title, substring)[0]
+	}
+
+	// Remove bracketed content
+	title = strings.TrimSpace(bracketedContent.ReplaceAllString(title, ""))
+
+	// Remove extra spaces
+	title = extraSpace.ReplaceAllString(title, " ")
+
+	// Split the title into words and remove the last word if it's 'documentary'
+	words := strings.Split(title, " ")
+	if strings.ToLower(words[len(words)-1]) == "documentary" {
+		words = words[1:]
+	}
+
+	// Iterate the words and mutate them
+	for i, w := range words {
+		// Convert word to runes
+		runes := []rune(w)
+
+		var fq string
+		var lq string
+
+		// Remove quotation marks from the word at start/end
+		// and store them for later use
+		if len(runes) > 1 {
+			if quotes[runes[0]] {
+				fq = string(runes[0])
+				w = string(runes[1:])
+			}
+
+			if quotes[runes[len(runes)-1]] {
+				lq = string(runes[len(runes)-1])
+				w = string(runes[:1])
+			}
+		}
+
+		// If not the first word try to lowercase the word
+		if i > 0 {
+			currentWord := strings.ToLower(w)
+			previousWord := []rune(words[i-1])
+			lastRune := previousWord[len(previousWord)-1]
+			// The word is a preposition but not after a punctuation
+			if preps[currentWord] && !puncts[lastRune] {
+				// Replace the actual word in the slice
+				words[i] = fq + currentWord + lq
+			}
+		}
+
+	}
+
+	return strings.Join(words, " ")
 }
