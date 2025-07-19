f2e0391d5719ea9c87da2b0b7979a65c0a62be22
vlatan <33163699+vlatan@users.noreply.github.com>
2025-07-19 12:37:19 +0200

    Provide more redis basic methods

--- internal/shared/redis/redis.go
+++ internal/shared/redis/redis.go
@@ -15,13 +15,20 @@ import (
 // Service represents a service that interacts with a redis client.
 type Service interface {
 	// Set a key-value pair in Redis with an expiration duration.
-	// The value will be marshaled to JSON if it's not a string or []byte.
 	Set(ctx context.Context, key string, value any, expiration time.Duration) error
 	// Get a value from Redis by key. Returns the value as a string.
 	// Returns redis.Nil error if the key does not exist.
 	Get(ctx context.Context, key string) (string, error)
 	// Delete value in Redis
 	Delete(ctx context.Context, key string) error
+	// Check if specific field exists in a hashmap
+	HExists(ctx context.Context, key, field string) (bool, error)
+	// Store hashmap
+	Hset(ctx context.Context, ttl time.Duration, key string, values ...any) error
+	// Get specific field from a hashmap
+	HGet(ctx context.Context, key, field string) (string, error)
+	// Get the entire hashmap
+	HGetAll(ctx context.Context, key string) (map[string]string, error)
 	// Get simple stats from the redis server
 	Health(ctx context.Context) map[string]any
 	// Close redis client
@@ -64,24 +71,52 @@ func (s *service) Get(ctx context.Context, key string) (string, error) {
 }
 
 // Set a key-value pair in Redis with an expiration duration.
-func (s *service) Set(ctx context.Context, key string, value any, expiration time.Duration) error {
+func (s *service) Set(ctx context.Context, key string, value any, ttl time.Duration) error {
 	switch v := value.(type) {
-	case string:
-		return s.rdb.Set(ctx, key, v, expiration).Err()
-	case []byte:
-		return s.rdb.Set(ctx, key, v, expiration).Err()
+	case string, []byte:
+		return s.rdb.Set(ctx, key, v, ttl).Err()
 	default:
 		jsonData, err := json.Marshal(value)
 		if err != nil {
 			return fmt.Errorf("failed to marshal value to JSON: %w", err)
 		}
-		return s.rdb.Set(ctx, key, jsonData, expiration).Err()
+		return s.rdb.Set(ctx, key, jsonData, ttl).Err()
 	}
 }
 
 // Delete a value in Redis
 func (s *service) Delete(ctx context.Context, key string) error {
 	return s.rdb.Del(ctx, key).Err()
+}
+
+// Store hashmap
+func (s *service) Hset(ctx context.Context, ttl time.Duration, key string, values ...any) error {
+	pipe := s.rdb.Pipeline()
+	if err := pipe.HSet(ctx, key, values...).Err(); err != nil {
+		return err
+	}
+
+	if err := pipe.Expire(ctx, key, ttl).Err(); err != nil {
+		return err
+	}
+
+	_, err := pipe.Exec(ctx)
+	return err
+}
+
+// Check if specific field exists in a hashmap
+func (s *service) HExists(ctx context.Context, key, field string) (bool, error) {
+	return s.rdb.HExists(ctx, key, field).Result()
+}
+
+// Get the entire hashmap
+func (s *service) HGetAll(ctx context.Context, key string) (map[string]string, error) {
+	return s.rdb.HGetAll(ctx, key).Result()
+}
+
+// Get specific field from a hashmap
+func (s *service) HGet(ctx context.Context, key, field string) (string, error) {
+	return s.rdb.HGet(ctx, key, field).Result()
 }
 
 // Check if the redis client is healthy
