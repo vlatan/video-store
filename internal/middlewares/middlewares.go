9c4ecb841c2a18456c858e2a9eb04af65b0bb5b9
vlatan <33163699+vlatan@users.noreply.github.com>
2025-08-01 08:56:18 +0200

    Remove headers

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -124,22 +124,18 @@ func (s *Service) RecoverPanic(next http.Handler) http.Handler {
 func (s *Service) AddHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// // Add CF cache control header if user not logged in AND not static file
-		// user := utils.GetUserFromContext(r)
-		// if !user.IsAuthenticated() && !utils.IsStatic(r) {
-		// 	w.Header().Set("CDN-Cache-Control", "14400")
-		// }
-
 		// Prevent MIME type sniffing
 		w.Header().Set("X-Content-Type-Options", "nosniff")
+
 		// XSS Protection
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
+
 		// Prevent clickjacking
 		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+
 		// HSTS (HTTPS only)
-		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
-		// Referrer Policy
-		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+
 		next.ServeHTTP(w, r)
 	})
 }
