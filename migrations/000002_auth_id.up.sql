91dca3fb80585464c97a68f935492a13e2038776
vlatan <33163699+vlatan@users.noreply.github.com>
2025-08-18 18:08:42 +0200

    Make the auth_id not null

--- migrations/000002_auth_id.up.sql
+++ migrations/000002_auth_id.up.sql
@@ -15,5 +15,6 @@ WHERE facebook_id IS NOT NULL;
 
 -- Add unique constraint on the combination of auth_id and provider
 -- This ensures a user can't have the same auth_id for the same provider
-ALTER TABLE app_user 
+ALTER TABLE app_user
+ALTER COLUMN auth_id SET NOT NULL,
 ADD CONSTRAINT app_user_auth_id_provider_key UNIQUE (auth_id, provider);
