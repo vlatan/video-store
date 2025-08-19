0a0fc8c93c0ba7b2b3a5e7a3048d4dd9406e3d6d
vlatan <33163699+vlatan@users.noreply.github.com>
2025-08-19 15:16:56 +0200

    Down migration is empty

--- migrations/000002_provider.down.sql
+++ migrations/000002_provider.down.sql
@@ -1,6 +1,4 @@
--- Remove the unique constraint
-ALTER TABLE app_user DROP CONSTRAINT IF EXISTS app_user_provider_user_id_provider_key;
 
--- Drop the new columns
-ALTER TABLE app_user DROP COLUMN IF EXISTS provider_user_id;
-ALTER TABLE app_user DROP COLUMN IF EXISTS provider;
+-- We are not deleting the newly added columns
+-- nor deleting the old ones 
+--- because this migration IS tied with a code change.
