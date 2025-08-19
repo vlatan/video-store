692c1d065c7c025623912d7c89323709a0ebb223
vlatan <33163699+vlatan@users.noreply.github.com>
2025-08-19 12:52:27 +0200

    Change provider id column and query

--- internal/repositories/users/queries.go
+++ internal/repositories/users/queries.go
@@ -1,37 +1,23 @@
 package users
 
 const upsertUserQuery = `
-	WITH existing_user AS (
-		SELECT id FROM app_user 
-		WHERE auth_id = $1
-	),
-	inserted AS (
-		INSERT INTO app_user (
-			auth_id, 
-			analytics_id, 
-			name, 
-			email, 
-			picture
-		)
-		SELECT $1, $2, $3, $4, $5
-		WHERE NOT EXISTS (SELECT 1 FROM existing_user)
-		RETURNING id
-	),
-	updated AS (
-		UPDATE app_user SET 
-			auth_id = COALESCE($1, auth_id),
-			analytics_id = COALESCE($2, analytics_id),
-			name = $3,
-			email = $4,
-			picture = $5,
-			last_seen = NOW()
-		FROM existing_user
-		WHERE app_user.id = existing_user.id
-		RETURNING app_user.id
+	INSERT INTO app_user (
+		provider_user_id, 
+		provider, 
+		analytics_id, 
+		name, 
+		email, 
+		picture, 
+		last_seen
 	)
-	SELECT id FROM inserted
-	UNION ALL
-	SELECT id FROM updated
+	VALUES ( $1, $2, $3, $4, $5, $6, NOW() ) 
+	ON CONFLICT (provider, provider_user_id) 
+	DO UPDATE SET 
+		name = EXCLUDED.name,
+		email = EXCLUDED.email,
+		picture = EXCLUDED.picture,
+		last_seen = NOW()
+	RETURNING id;
 `
 
 const deleteUserQuery = "DELETE FROM app_user WHERE id = $1"
@@ -40,7 +26,7 @@ const updateLastUserSeenQuery = "UPDATE app_user SET last_seen = $2 WHERE id = $
 
 const getUsersQuery = `
 	SELECT
-		auth_id,
+		provider_user_id,
 		provider, 
 		name,
 		email,
--- migrations/000002_auth_id.down.sql
+++ migrations/000002_auth_id.down.sql
@@ -1,6 +1,6 @@
 -- Remove the unique constraint
-ALTER TABLE app_user DROP CONSTRAINT IF EXISTS app_user_auth_id_provider_key;
+ALTER TABLE app_user DROP CONSTRAINT IF EXISTS app_user_provider_user_id_provider_key;
 
 -- Drop the new columns
-ALTER TABLE app_user DROP COLUMN IF EXISTS auth_id;
+ALTER TABLE app_user DROP COLUMN IF EXISTS provider_user_id;
 ALTER TABLE app_user DROP COLUMN IF EXISTS provider;
