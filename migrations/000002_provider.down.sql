9fa907e914a72d7d78cc603f2dcc9b1d19163f97
vlatan <33163699+vlatan@users.noreply.github.com>
2025-08-19 15:59:34 +0200

    Keep the down migration with warning

--- migrations/000002_provider.down.sql
+++ migrations/000002_provider.down.sql
@@ -1,5 +1,12 @@
+-- There's a app code change already DONE.
+-- If you run this expect the app to break.
+-- The app expects to find the `provider` and `provider_user_id` columns.
 
--- We are not deleting the newly added columns,
--- nor deleting the old ones,
--- because this migration IS tied with a code change.
--- We want to be able to revert back the code safely if necessary.
+-- Remove the unique constraint
+ALTER TABLE app_user 
+DROP CONSTRAINT app_user_provider_user_id_provider_key;
+
+-- Drop the new columns
+ALTER TABLE app_user
+DROP COLUMN provider_user_id,
+DROP COLUMN provider;
