70f8416860413c270c081c178bf4ace21e579ffa
vlatan <33163699+vlatan@users.noreply.github.com>
2025-08-19 15:31:45 +0200

    Add comments

--- migrations/000002_provider.down.sql
+++ migrations/000002_provider.down.sql
@@ -1,4 +1,5 @@
 
--- We are not deleting the newly added columns
--- nor deleting the old ones 
+-- We are not deleting the newly added columns,
+-- nor deleting the old ones,
 -- because this migration IS tied with a code change.
+-- We want to be able to revert back the code safely if necessary.
