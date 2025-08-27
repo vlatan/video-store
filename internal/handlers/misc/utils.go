fe13716d805057be369e5e20dce25d382f0e4977
vlatan <33163699+vlatan@users.noreply.github.com>
2025-08-27 12:45:44 +0200

    Add server stats

--- internal/handlers/misc/utils.go
+++ internal/handlers/misc/utils.go
@@ -1,6 +1,14 @@
 package misc
 
-import "runtime"
+import (
+	"math"
+	"runtime"
+)
+
+func bToMib(bytes uint64) float64 {
+	mib := float64(bytes) / (1024 * 1024)
+	return math.Round(mib*100) / 100
+}
 
 // Get basic server stats
 func getServerStats() map[string]any {
@@ -8,10 +16,15 @@ func getServerStats() map[string]any {
 	runtime.ReadMemStats(&m)
 
 	return map[string]any{
-		"goroutines":   runtime.NumGoroutine(),
-		"memory_alloc": m.Alloc,
-		"memory_sys":   m.Sys,
-		"gc_runs":      m.NumGC,
-		"cpu_count":    runtime.NumCPU(),
+		"num_cpu":          runtime.NumCPU(),
+		"num_gc":           m.NumGC,
+		"num_goroutine":    runtime.NumGoroutine(),
+		"gomaxprocs":       runtime.GOMAXPROCS(0),
+		"mem_alloc_MB":     bToMib(m.Alloc),
+		"mem_sys_MB":       bToMib(m.Sys),
+		"mem_heap_sys_MB":  bToMib(m.HeapSys),
+		"mem_stack_sys_MB": bToMib(m.StackSys),
+		"mem_gc_sys_MB":    bToMib(m.GCSys),
+		"mem_other_sys_MB": bToMib(m.OtherSys),
 	}
 }
