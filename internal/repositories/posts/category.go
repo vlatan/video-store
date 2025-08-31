5dafa1db32cd4949af6cfe5d2537940010e965f1
vlatan <33163699+vlatan@users.noreply.github.com>
2025-08-31 16:23:56 +0200

    Factor out taxonomy fetch from DB

--- internal/repositories/posts/category.go
+++ internal/repositories/posts/category.go
@@ -2,11 +2,7 @@ package posts
 
 import (
 	"context"
-	"encoding/base64"
-	"errors"
 	"factual-docs/internal/models"
-	"fmt"
-	"time"
 )
 
 const getCategoryPostsQuery = `
@@ -39,65 +35,17 @@ func (r *Repository) GetCategoryPosts(
 	orderBy string,
 ) (*models.Posts, error) {
 
-	// Construct the SQL parts as well as the arguments
-	// The category slug and limit are the first two arguments ($1 and $2)
-	// Peek for one post beoynd the limit
-	var where string
-	args := []any{categorySlug, r.config.PostsPerPage + 1}
-	order := "upload_date DESC, id DESC"
-	if orderBy == "likes" {
-		order = "likes DESC, " + order
-	}
+	posts, err := r.queryTaxonomyPosts(
+		ctx,
+		getCategoryPostsQuery,
+		categorySlug,
+		cursor,
+		orderBy,
+	)
 
-	// Build args and SQL parts
-	if cursor != "" {
-
-		cursorParts, err := decodeCursor(cursor)
-		if err != nil {
-			return nil, err
-		}
-
-		switch orderBy {
-		case "likes":
-			if len(cursorParts) != 3 {
-				return nil, errors.New("invalid cursor components")
-			}
-			args = append(args, cursorParts[0], cursorParts[1], cursorParts[2])
-			where = "WHERE (likes, upload_date, id) < ($3, $4, $5)"
-		default:
-			if len(cursorParts) != 2 {
-				return nil, fmt.Errorf("invalid cursor components")
-			}
-			args = append(args, cursorParts[0], cursorParts[1])
-			where = "WHERE (upload_date, id) < ($3, $4)"
-		}
-	}
-
-	query := fmt.Sprintf(getCategoryPostsQuery, where, order)
-	posts, err := r.queryTaxonomyPosts(ctx, query, args...)
 	if err != nil {
 		return nil, err
 	}
-
-	// This is the last page
-	if len(posts.Items) <= r.config.PostsPerPage {
-		return posts, err
-	}
-
-	// Exclude the last post
-	posts.Items = posts.Items[:len(posts.Items)-1]
-
-	// Determine the next cursor
-	lastPost := posts.Items[len(posts.Items)-1]
-	uploadDate := lastPost.UploadDate.Format(time.RFC3339Nano)
-	cursorStr := fmt.Sprintf("%s,%d", uploadDate, lastPost.ID)
-
-	// If ordering is by likes
-	if orderBy == "likes" {
-		cursorStr = fmt.Sprintf("%d,%s", lastPost.Likes, cursorStr)
-	}
-
-	posts.NextCursor = base64.StdEncoding.EncodeToString([]byte(cursorStr))
 
 	return posts, err
 }
--- internal/repositories/posts/utils.go
+++ internal/repositories/posts/utils.go
@@ -9,18 +9,54 @@ import (
 	"factual-docs/internal/utils"
 	"fmt"
 	"strings"
+	"time"
 )
 
 // Query the DB for posts based on variadic arguments
 func (r *Repository) queryTaxonomyPosts(
 	ctx context.Context,
-	query string,
-	args ...any,
+	query,
+	taxonomyID,
+	cursor,
+	orderBy string,
 ) (*models.Posts, error) {
 
-	var posts models.Posts
+	// Construct the SQL parts as well as the arguments
+	// The category slug and limit are the first two arguments ($1 and $2)
+	// Peek for one post beoynd the limit
+	var where string
+	args := []any{taxonomyID, r.config.PostsPerPage + 1}
+	order := "upload_date DESC, id DESC"
+	if orderBy == "likes" {
+		order = "likes DESC, " + order
+	}
+
+	// Build args and SQL parts
+	if cursor != "" {
+
+		cursorParts, err := decodeCursor(cursor)
+		if err != nil {
+			return nil, err
+		}
+
+		switch orderBy {
+		case "likes":
+			if len(cursorParts) != 3 {
+				return nil, errors.New("invalid cursor components")
+			}
+			args = append(args, cursorParts[0], cursorParts[1], cursorParts[2])
+			where = "WHERE (likes, upload_date, id) < ($3, $4, $5)"
+		default:
+			if len(cursorParts) != 2 {
+				return nil, fmt.Errorf("invalid cursor components")
+			}
+			args = append(args, cursorParts[0], cursorParts[1])
+			where = "WHERE (upload_date, id) < ($3, $4)"
+		}
+	}
 
 	// Get rows from DB
+	query = fmt.Sprintf(query, where, order)
 	rows, err := r.db.Query(ctx, query, args...)
 	if err != nil {
 		return nil, err
@@ -30,6 +66,7 @@ func (r *Repository) queryTaxonomyPosts(
 	defer rows.Close()
 
 	// Iterate over the rows
+	var posts models.Posts
 	for rows.Next() {
 		var post models.Post
 		var thumbnails []byte
@@ -68,6 +105,26 @@ func (r *Repository) queryTaxonomyPosts(
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
+
+	// This is the last page
+	if len(posts.Items) <= r.config.PostsPerPage {
+		return &posts, err
+	}
+
+	// Exclude the last post
+	posts.Items = posts.Items[:len(posts.Items)-1]
+
+	// Determine the next cursor
+	lastPost := posts.Items[len(posts.Items)-1]
+	uploadDate := lastPost.UploadDate.Format(time.RFC3339Nano)
+	cursorStr := fmt.Sprintf("%s,%d", uploadDate, lastPost.ID)
+
+	// If ordering is by likes
+	if orderBy == "likes" {
+		cursorStr = fmt.Sprintf("%d,%s", lastPost.Likes, cursorStr)
+	}
+
+	posts.NextCursor = base64.StdEncoding.EncodeToString([]byte(cursorStr))
 
 	return &posts, nil
 }
