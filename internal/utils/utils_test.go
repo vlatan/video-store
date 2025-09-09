02fdde92220ef8aebb31d138a2475942b0bdb89d
vlatan <33163699+vlatan@users.noreply.github.com>
2025-09-09 16:48:25 +0200

    Add test func

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -1,8 +1,42 @@
 package utils
 
 import (
+	"context"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/vlatan/video-store/internal/models"
 )
+
+func TestGetUserFromContext(t *testing.T) {
+
+	var user = &models.User{ID: 1, Name: "test"}
+
+	tests := []struct {
+		name     string
+		user     *models.User
+		expected *models.User
+	}{
+		{"user in context", user, user},
+		{"no user in context", nil, nil},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/", nil)
+			// Add user to context if not nil
+			if tt.user != nil {
+				ctx := context.WithValue(req.Context(), UserContextKey, tt.user)
+				req = req.WithContext(ctx)
+			}
+
+			result := GetUserFromContext(req)
+			if result != tt.expected {
+				t.Errorf("got %v, want %v", result, tt.expected)
+			}
+		})
+	}
+}
 
 func TestValidateFilePath(t *testing.T) {
 
