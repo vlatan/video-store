ed0233c98cf4bc79c1ce6707ad309eb1f67a8410
vlatan <33163699+vlatan@users.noreply.github.com>
2025-09-14 11:02:48 +0200

    Test thumbnails equal

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -293,8 +293,9 @@ func TestPlural(t *testing.T) {
 
 func TestThumbnailEqual(t *testing.T) {
 
-	a := &models.Thumbnail{Width: 10, Height: 5, Url: "foo"}
-	b := &models.Thumbnail{Width: 10, Height: 5, Url: "bar"}
+	a := models.Thumbnail{Width: 10, Height: 5, Url: "foo"}
+	b := a
+	b.Url = "bar"
 
 	tests := []struct {
 		name     string
@@ -303,15 +304,55 @@ func TestThumbnailEqual(t *testing.T) {
 		expected bool
 	}{
 		{"nil structs", nil, nil, true},
-		{"first nil struct", nil, a, false},
-		{"second nil struct", a, nil, false},
-		{"different structs", a, b, false},
-		{"identical structs", a, a, true},
+		{"first nil struct", nil, &a, false},
+		{"second nil struct", &a, nil, false},
+		{"different structs", &a, &b, false},
+		{"identical structs", &a, &a, true},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := ThumbnailEqual(tt.a, tt.b)
+			if got != tt.expected {
+				t.Errorf("got %t, want %t", got, tt.expected)
+			}
+		})
+	}
+}
+
+func TestThumbnailsEqual(t *testing.T) {
+
+	a := models.Thumbnail{Width: 10, Height: 5, Url: "foo"}
+	b := a
+	b.Url = "bar"
+
+	thumbsA := models.Thumbnails{
+		Default:  &a,
+		Medium:   &b,
+		High:     &a,
+		Standard: &b,
+		Maxres:   &a,
+	}
+
+	thumbsB := thumbsA
+	thumbsB.Maxres = &b
+
+	tests := []struct {
+		name     string
+		thumbsA  *models.Thumbnails
+		thumbsB  *models.Thumbnails
+		expected bool
+	}{
+		{"nil structs", nil, nil, true},
+		{"first nil struct", nil, &thumbsA, false},
+		{"second nil struct", &thumbsA, nil, false},
+		{"different structs", &thumbsA, &thumbsB, false},
+		{"identical structs", &thumbsA, &thumbsA, true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ThumbnailsEqual(tt.thumbsA, tt.thumbsB)
 			if got != tt.expected {
 				t.Errorf("got %t, want %t", got, tt.expected)
 			}
