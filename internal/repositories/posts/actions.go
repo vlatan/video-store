93f9c741d7b15d03796eababeddab19b3b4461a0
vlatan <33163699+vlatan@users.noreply.github.com>
2025-11-29 12:39:50 +0100

    Fix update playlist query

--- internal/repositories/posts/actions.go
+++ internal/repositories/posts/actions.go
@@ -102,7 +102,11 @@ const updatePlaylistQuery = `
 	UPDATE post
 	SET 
 		playlist_id = $2,
-		playlist_db_id = (SELECT id FROM playlist WHERE playlist_id = $2)
+		playlist_db_id = (
+			SELECT id
+			FROM playlist
+			WHERE playlist_id = $2::VARCHAR(50)
+		)
 	WHERE video_id = $1
 `
 
