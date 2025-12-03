df42e89f5ff96e74546090f856c66cd321f5eb36
vlatan <33163699+vlatan@users.noreply.github.com>
2025-12-03 17:31:54 +0100

    Stabilize sitemap query

--- internal/repositories/posts/sitemap.go
+++ internal/repositories/posts/sitemap.go
@@ -7,12 +7,19 @@ import (
 	"github.com/vlatan/video-store/internal/models"
 )
 
+// The query selects the entire DB data.
+// Also it forms pages that are not in DB but created on the fly by the app.
+// The data is sorted by the created_at column which creates a stable table each time,
+// appending the newest additions at the end.
+// So when this data is split in parts each part always has the same data,
+// except the last part which swells.
 const sitemapDataQuery = `
 	-- Posts (last modified = last updated_at)
 	SELECT
 		'post' as type,
 		CONCAT('/video/', video_id, '/') AS url,
-		updated_at
+		updated_at,
+		created_at
 	FROM post
 
 	UNION ALL
@@ -21,7 +28,8 @@ const sitemapDataQuery = `
 	SELECT
 		'page' AS type,
 		CONCAT('/page/', slug, '/') AS url,
-		updated_at
+		updated_at,
+		created_at
 	FROM page
 
 	UNION ALL
@@ -30,10 +38,11 @@ const sitemapDataQuery = `
 	SELECT
 		'source' AS type, 
 		CONCAT('/source/', p.playlist_id, '/') AS url, 
-		MAX(post.upload_date) AS updated_at
+		MAX(post.upload_date) AS updated_at,
+		p.created_at
 	FROM playlist AS p
 	LEFT JOIN post ON post.playlist_db_id = p.id
-	GROUP BY p.id
+	GROUP BY p.id, p.created_at
 
 	UNION ALL
 
@@ -41,7 +50,8 @@ const sitemapDataQuery = `
 	SELECT
 		'source' AS type,
 		'/source/other/' AS url,
-		MAX(post.upload_date) AS updated_at
+		MAX(upload_date) AS updated_at,
+		MIN(created_at) AS created_at
 	FROM post
 	WHERE playlist_id IS NULL OR playlist_id = ''
 
@@ -51,10 +61,11 @@ const sitemapDataQuery = `
 	SELECT
 		'category' AS type,
 		CONCAT('/category/', c.slug, '/') AS url,
-		MAX(post.upload_date) AS updated_at
+		MAX(post.upload_date) AS updated_at,
+		c.created_at
 	FROM category AS c
 	LEFT JOIN post ON post.category_id = c.id
-	GROUP BY c.id
+	GROUP BY c.id, c.created_at
 
 	UNION ALL
 
@@ -62,7 +73,8 @@ const sitemapDataQuery = `
 	SELECT
 		'misc' AS type,
 		'/' AS url,
-		MAX(upload_date) AS updated_at
+		MAX(upload_date) AS updated_at,
+		MIN(created_at) AS created_at
 	FROM post
 
 	UNION ALL
@@ -71,8 +83,11 @@ const sitemapDataQuery = `
 	SELECT 
 		'misc' AS type,
 		'/sources/' AS url, 
-		MAX(created_at) AS updated_at
+		MAX(created_at) AS updated_at,
+		MIN(created_at) AS created_at
 	FROM playlist
+
+	ORDER BY created_at ASC, url ASC
 `
 
 func (r *Repository) SitemapData(ctx context.Context) ([]*models.SitemapItem, error) {
@@ -90,10 +105,10 @@ func (r *Repository) SitemapData(ctx context.Context) ([]*models.SitemapItem, er
 	var data []*models.SitemapItem
 	for rows.Next() {
 		var item models.SitemapItem
-		var lastModified *time.Time
+		var lastModified, createdAt *time.Time
 
 		// Paste post from row to struct, thumbnails in a separate var
-		if err = rows.Scan(&item.Type, &item.Location, &lastModified); err != nil {
+		if err = rows.Scan(&item.Type, &item.Location, &lastModified, &createdAt); err != nil {
 			return nil, err
 		}
 
