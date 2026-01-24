996a2bc740eefe44023798c99f6d2da6ff527795
vlatan <33163699+vlatan@users.noreply.github.com>
2026-01-24 17:56:39 +0100

    Move main tests

--- internal/integrations/gemini/gemini_test.go
+++ internal/integrations/gemini/gemini_test.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/vlatan/video-store/internal/config"
-	"github.com/vlatan/video-store/internal/models"
 	"github.com/vlatan/video-store/internal/testutils"
 )
 
@@ -58,51 +57,4 @@ func runTests(m *testing.M) int {
 
 	// Run all the tests in the package
 	return m.Run()
-}
-
-func TestParseResponse(t *testing.T) {
-
-	categories := models.Categories{
-		models.Category{Name: "Science"},
-	}
-
-	tests := []struct {
-		name       string
-		raw        string
-		categories models.Categories
-		wantErr    bool
-		expected   *models.GenaiResponse
-	}{
-		{
-			"valid test",
-			"<p>Foo Bar</p>\n <p>Foo Bar</p> CATEGORY: Science",
-			categories,
-			false,
-			&models.GenaiResponse{
-				Summary:  "<p>Foo Bar</p>\n <p>Foo Bar</p>",
-				Category: "Science",
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			response, err := parseResponse(tt.raw, tt.categories)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("got error = %v, want error = %v", err, tt.wantErr)
-			}
-
-			if response.Summary != tt.expected.Summary {
-				t.Errorf("got summary %q, want summary %q",
-					response.Summary, tt.expected.Summary,
-				)
-			}
-
-			if response.Category != tt.expected.Category {
-				t.Errorf("got category %q, want category %q",
-					response.Category, tt.expected.Category,
-				)
-			}
-		})
-	}
 }
