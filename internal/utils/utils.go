0747839b3d39fcf2dbac34ab67fb1479590ed13a
vlatan <33163699+vlatan@users.noreply.github.com>
2026-05-15 14:37:07 +0200

    Change marker

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -29,7 +29,7 @@ var RootFavicons = []string{
 	"/site.webmanifest",
 }
 
-const UpdateMarker = "<!-- v2 -->" // REMOVE
+const UpdateMarker = "<!-- v3 -->" // REMOVE
 
 // Create canonical URI string
 func CanonicalURL(r *http.Request, protocol string) string {
