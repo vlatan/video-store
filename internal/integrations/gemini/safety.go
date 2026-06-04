1a548f751ebe1858458bbb379666ec057c13c832
vlatan <33163699+vlatan@users.noreply.github.com>
2026-06-04 13:03:43 +0200

    Rearrange safety settings

--- internal/integrations/gemini/safety.go
+++ internal/integrations/gemini/safety.go
@@ -5,8 +5,8 @@ import "google.golang.org/genai"
 // Configure safety settings to block none
 var blockNone = genai.HarmBlockThresholdBlockNone
 var safetySettings = []*genai.SafetySetting{
-	{Category: genai.HarmCategoryHateSpeech, Threshold: blockNone},
-	{Category: genai.HarmCategoryDangerousContent, Threshold: blockNone},
 	{Category: genai.HarmCategoryHarassment, Threshold: blockNone},
+	{Category: genai.HarmCategoryHateSpeech, Threshold: blockNone},
 	{Category: genai.HarmCategorySexuallyExplicit, Threshold: blockNone},
+	{Category: genai.HarmCategoryDangerousContent, Threshold: blockNone},
 }
