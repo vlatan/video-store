a126ab5cfe6cf0307b79983c613e01af5265f2b9
vlatan <33163699+vlatan@users.noreply.github.com>
2026-07-03 17:58:12 +0200

    Fix cats repo argument

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -59,7 +59,7 @@ func New(cfg *config.Config, ctx context.Context) (*Worker, error) {
 		return nil, fmt.Errorf("couldn't create sources repo: %w", err)
 	}
 
-	catsRepo, err := categories.New(db)
+	catsRepo, err := categories.New(db, nil)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't create categories repo: %w", err)
 	}
