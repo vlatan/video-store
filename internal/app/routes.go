7e65b44b86bd5f297ffd516861aac1586a9fc14a
vlatan <33163699+vlatan@users.noreply.github.com>
2026-09-18 20:13:20 +0200

    Edit comment

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -102,7 +102,7 @@ func (a *App) RegisterRoutes() *App {
 	// The order is important.
 	// Use this custom handler as HTTP server handler.
 	a.server.Handler = a.mw.ApplyToAll(
-		a.mw.CloseBody,                          // Absolute safety net for body memory leaks
+		a.mw.CloseBody,                          // Safety net for body memory leaks
 		a.mw.Compress,                           // Compress the response no matter what is it
 		a.mw.CanonicalRedirect,                  // Redirect www to non-www
 		a.mw.MethodOverride,                     // Override a POST method if needed
