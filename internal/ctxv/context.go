5d36cae94c54a820944d61d653a17b21f36b5007
vlatan <33163699+vlatan@users.noreply.github.com>
2026-09-23 12:50:37 +0200

    Add info to func comment

--- internal/ctxv/context.go
+++ internal/ctxv/context.go
@@ -13,7 +13,8 @@ func Get[T any](ctx context.Context) T {
 	return val // zero value (nil for pointers) if not present
 }
 
-// WithValue adds value to context and returns the new context
+// WithValue adds value to context and returns the new context.
+// Do not use primitive types, create new type definition for the ctx key to be unique.
 func WithValue[T any](ctx context.Context, val T) context.Context {
 	return context.WithValue(ctx, ctxKey[T]{}, val)
 }
